90b4beabb961cbbfe9e9ee1d622b2b4d16317162
Nermin Jukan <32697475+nerminjukan@users.noreply.github.com>
2024-05-08 00:08:37 +0200

    add materials, api service

---
+++ mockup/src/app/api.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { Subject } from 'rxjs/Subject';
+// import { environment } from '../../environments/environment';
+
+@Injectable()
+export class ApiService {
+  
+}
